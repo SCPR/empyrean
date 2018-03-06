d582338a890ce73d2e2d51916697a144952dd030
Ben Titcomb <btitcomb@scpr.org>
2018-03-06 12:25:37 -0800

    A little aesthetic cleanup for index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 'use strict';
-const fs        = require('fs');
-const YAML      = require('js-yaml');
-const AWS       = require('aws-sdk');
-const PouchDB   = require('pouchdb');
-PouchDB.plugin(require('pouchdb-upsert'));
-const GrandCentral  = require('./lib/grand-central');
-const loadAdapters  = require('./lib/adapter-loader');
-const Logger        = require('./lib/logger');
+const fs           = require('fs'),
+      YAML         = require('js-yaml'),
+      AWS          = require('aws-sdk'),
+      PouchDB      = require('pouchdb'),
+      PouchDB.plugin(require('pouchdb-upsert')),
+      GrandCentral = require('./lib/grand-central'),
+      loadAdapters = require('./lib/adapter-loader'),
+      Logger       = require('./lib/logger'),
+      mode         = require('./lib/what-mode')();
 
-const mode          = require('./lib/what-mode')();
-
-let secrets   = YAML.safeLoad(fs.readFileSync('./secrets.yml', 'utf8'))[mode];
-
-let config    = YAML.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
-
-let validator = require('./lib/schema-validator'); 
+let secrets   = YAML.safeLoad(fs.readFileSync('./secrets.yml', 'utf8'))[mode],
+    config    = YAML.safeLoad(fs.readFileSync('./config.yml', 'utf8')),
+    validator = require('./lib/schema-validator'); 
 
 config.sqs.queues.messages.QueueUrl = secrets.aws.sqs.queue_url;
 
 AWS.config.update({accessKeyId: secrets.aws.access_key_id, secretAccessKey: secrets.aws.secret_access_key, region: secrets.aws.region});
 
-let adapters = loadAdapters('./adapters', secrets);
+let adapters = loadAdapters('./adapters', secrets),
 
-let gc      = new GrandCentral({
+gc = new GrandCentral({
   db:       new PouchDB(secrets.pouchdb.database),
   sqs:      new AWS.SQS({apiVersion: '2012-11-05'}),
   adapters: adapters,
