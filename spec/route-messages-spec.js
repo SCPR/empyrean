06bfaf234931d1d8c506ddf5de53421bf99c4d92
Ben Titcomb <btitcomb@scpr.org>
2018-03-06 12:57:06 -0800

    Update tests to use memory adapter for PouchDB.

--- spec/route-messages-spec.js
+++ spec/route-messages-spec.js
@@ -1,37 +1,32 @@
 'use strict';
 
-const fs      = require('fs');
-const YAML    = require('js-yaml');
-const fakeSQS = require('./mocks/sqs');
-const PouchDB = require('pouchdb');
-const GrandCentral  = require('../lib/grand-central');
-const Logger    = require('../lib/logger');
+const fs      = require('fs'),
+      YAML    = require('js-yaml'),
+      fakeSQS = require('./mocks/sqs'),
+      PouchDB = require('pouchdb'),
+      GrandCentral  = require('../lib/grand-central'),
+      Logger        = require('../lib/logger'),
+      mode          = "test";
 
 PouchDB.plugin(require('pouchdb-upsert'));
 PouchDB.plugin(require('pouchdb-erase'));
+PouchDB.plugin(require('pouchdb-adapter-memory'));
 
-const mode      = "test";
+let secrets     = YAML.safeLoad(fs.readFileSync('./secrets.yml', 'utf8'))[mode],
+    adapters    = {myspace: require('./mocks/adapters/myspace')({}), missing: require('../adapters/missing')({})},
+    validator   = require('../lib/schema-validator'), 
+    db          = new PouchDB('grand-central-test', {adapter: 'memory'}),
+    sqs         = new fakeSQS([]),
+    gc          = new GrandCentral({
+      db:        db,
+      sqs:       sqs,
+      adapters:  adapters,
+      logger:    new Logger(mode),
+      validator: validator,
+      config:    YAML.safeLoad(fs.readFileSync('./config.yml', 'utf8'))
+    });
 
-let secrets     = YAML.safeLoad(fs.readFileSync('./secrets.yml', 'utf8'))[mode];
-
-let adapters    = {myspace: require('./mocks/adapters/myspace')({}), missing: require('../adapters/missing')({})};
-
-let validator   = require('../lib/schema-validator'); 
-
-let db          = new PouchDB(secrets.pouchdb.database);
-
-let sqs         = new fakeSQS([]);
-
-let gc          = new GrandCentral({
-  db:        db,
-  sqs:       sqs,
-  adapters:  adapters,
-  logger:    new Logger(mode),
-  validator: validator,
-  config:    YAML.safeLoad(fs.readFileSync('./config.yml', 'utf8'))
-});
-
-let generateTestMessage = (opts) => {
+const generateTestMessage = (opts) => {
   let message = YAML.safeLoad(fs.readFileSync('./spec/fixtures/test-message.yml', 'utf8'));
   opts = opts || {};
   Object.keys(opts).forEach((k) => {
@@ -50,29 +45,35 @@ describe("routeMessages", function() {
     });
   });
 
-  it("writes a successful push to the database", (done) => {
+  it("writes a successful push to the database", done => {
     let testMessage = generateTestMessage();
     gc.routeMessages([testMessage]).then(() => {
-      gc.db.get(testMessage.MessageAttributes._id.StringValue).then((doc) => {
+      return gc.db.get(testMessage.MessageAttributes._id.StringValue).then(doc => {
         expect(doc.syndications.myspace.code).toEqual(201);
-        done();
-      }).catch((err) => {
-        done.fail(err);
       });
+    })
+    .catch(err => {
+      done.fail(err);
+    })
+    .then(() => {
+      done();
     });
   });
 
   describe("a nonexistent adapter", () => {
-    it("writes a status of 409 to the syndication record", (done) => {
+    it("writes a status of 409 to the syndication record", done => {
       let message = generateTestMessage();
       message.MessageAttributes.adapter.StringValue = 'gooblegobble';
       gc.routeMessages([message]).then(() => {
-        gc.db.get(message.MessageAttributes._id.StringValue).then((doc) => {
+        return gc.db.get(message.MessageAttributes._id.StringValue).then(doc => {
           expect(doc.syndications.gooblegobble.code).toEqual(409);
-          done();
-        }).catch((err) => {
-          done.fail(err);
         });
+      })
+      .catch(err => {
+        done.fail(err);
+      })
+      .then(() => {
+        done();
       });
     });
   });
